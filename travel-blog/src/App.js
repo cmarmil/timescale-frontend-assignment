ae2a6a45e20bbcf4642435858a17bf94f314b964
cmiller <cmiller@graphiclanguage.net>
2020-11-17 14:24:59 -0500

    navbar markup and styling

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -1,10 +1,12 @@
 import headshot from './images/headshot.jpg';
 import heroBackground from './images/hero-background.jpg';
+import Navbar from './navbar';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
+      <Navbar/>
     </div>
   );
 }
---
+++ travel-blog/src/navbar.js
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar--col">
+        <div class="titleContainer">
+          <h3 className="navbar--title">Travelize</h3>
+          <p className="navbar--subtitle">
+            <em>My Traveling Experiences</em>
+          </p>
+        </div>
+      </div>
+      <div className="navbar--col">
+        <button className="button">Home</button>
+        <button className="button">Blog</button>
+        <button className="button button--filled button--state-primary">About</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
